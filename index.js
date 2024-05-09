36d4304bf167544b08aa50795d156ab002624e12
Yuri Matheus <yure1921@outlook.com>
2024-05-09 07:32:27 -0300

    teste

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-    console.log('TESTE')
+    let fome = prompt("você está com fome? (sim/nao)");
+    let dinheiro = prompt ("você está com dinheiro? (sim/nao)");
+    let restaurante = prompt ("O restaurante está aberto? (sim/nao)");
+
+    if (fome === "nao" || dinheiro === "nao"){
+        console.log("Hoje o jantar será em casa");
+    }else if(fome = "sim" && dinheiro === "sim"){
+        if(restaurante === "sim"){
+            console.log("Hoje você vai no seu restaurante preferido");
+        }else{
+            console.log("Peça um delivery");
+        }
+    }
